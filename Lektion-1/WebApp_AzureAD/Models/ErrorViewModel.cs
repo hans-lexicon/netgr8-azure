66ae2e43aab5ad93840e47f6e7fb7caf3c6d4723
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2022-06-15 07:08:29 +0200

    lektion 1 (Key Vault, Identity, Azure AD)

---
+++ Lektion-1/WebApp_AzureAD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApp_AzureAD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Lektion-1/WebApp_Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApp_Identity.Models;
+
+namespace WebApp_Identity.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
